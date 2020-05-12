d61eb74622fb15e6f62ff45a013c47e2917737ed
RandonY <64897746+RandonY@users.noreply.github.com>
2020-05-12 19:07:09 +0200

    Update Application.py

--- Application.py
+++ Application.py
@@ -38,7 +38,7 @@ class App(tk.Tk):
         
         self.frames = {}
         
-        for F in (HomePage, PageOne, Graphs):
+        for F in (HomePage, PageOne, Graphs, Graph_dynam):
 
             frame = F(container, self)
             
@@ -82,7 +82,10 @@ class PageOne(tk.Frame):
         
         button_home = ttk.Button(self, text = "Home Button", 
                                      command = lambda: controller.show_frame(HomePage))
+        button_bar_chart = ttk.Button(self, text = "Display plot chart", 
+                                     command = lambda: controller.show_frame(Graph_dynam))
         button_home.pack()
+        button_bar_chart.pack()
         
 
 class Graphs(tk.Frame):
@@ -116,6 +119,116 @@ class Graphs(tk.Frame):
         canvas.draw()
         canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
         
+class Graph_dynam(tk.Frame):
+    
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Kraken Graph")
+        label.pack(pady=10, padx=10)
+        
+            
+        x = []   #temps
+        y = []   #price_list
+        nb_plot_counter = 0
+        update_graph = False
+        update_scrap = False
+            
+        def on_click_button_updating_scrap(event):
+            global update_scrap
+            if update_scrap:
+                update_scrap = False
+                button_updating_scrap_var.set("Launch scrap")
+            else:
+                update_scrap = True
+                button_updating_scrap_var.set("Stop scrap")
+                global thread_1
+                thread_1 = Thread(target=scrap_thread)
+                thread_1.start()
+            print("quelqu'un a cliqué scrap", event.x, event.y)
+                
+        def my_personal_spyder_scraping():
+            PATH = "https://trade.kraken.com/fr-fr/charts/KRAKEN:BTC-USD"
+            driver = webdriver.Firefox()
+            driver.get(PATH)
+            for i in range(5):
+                time.sleep(5)
+                price = driver.find_element_by_class_name("price")
+                print(price.text)
+                y.append(float(price.text))
+                current_time = time.localtime()
+                x.append(time.strftime('%H:%M:%S', current_time))
+            driver.close()
+                
+        def scrap_thread():
+            global update_scrap
+            print("je lance mon thread")
+            while True:
+                if update_scrap:
+                    my_personal_spyder_scraping()
+                else:
+                    break
+            print("j'ai fini mon thread")
+                
+        def on_click_button_updating_graph(event):
+            global update_graph
+            if update_graph:
+                update_graph = False
+                button_updating_graph_var.set("Launch graph")
+            else:
+                update_graph = True
+                button_updating_graph_var.set("Stop graph")
+                refresh()
+            
+            print("quelqu'un a cliqué", event.x, event.y)
+            
+        def refresh():
+            global update_graph    
+            if not update_graph:
+                print("je m'arrete")
+                return
+            global nb_plot_counter
+            print("I'm refresh")
+            a.lines.pop(0)  # detruit l'ancienne ligne
+            nb_plot_counter += 1
+            a.plot(x[:nb_plot_counter], y[:nb_plot_counter])  # créer une nouvelle ligne
+            a.set_xticks(x)
+            a.set_xticklabels(x, rotation = 60)
+            canvas.draw()
+            self.after(2000, refresh)  # call la fonction apres 2000 ms
+        
+                
+                
+        
+        button_updating_graph_var = tk.StringVar()
+        button_updating_graph = tk.Button(self, textvariable=button_updating_graph_var) #on déclare le bouton et on le bind au tk.StringVar()
+        button_updating_graph.pack(side='top')
+        button_updating_graph_var.set("Launch graph")
+        
+        button_updating_scrap_var = tk.StringVar()
+        button_updating_scrap = tk.Button(self, textvariable=button_updating_scrap_var)
+        button_updating_scrap.pack(side='top')
+        button_updating_scrap_var.set("Launch scrap")
+        
+        button_home = ttk.Button(self, text = "Home Button", 
+                                     command = lambda: controller.show_frame(HomePage))
+        
+        f = Figure(figsize=(5,5), dpi=100)
+        a = f.add_subplot(111)
+        """selenium_btceur_kraken_scraping()"""
+        a.plot([], [])
+        
+        """a.set_xticks(x)
+        a.set_yticks(y)
+        a.set_xticklabels(x, rotation = 60)"""
+        a.set_ylabel("valeur de BTC en €")
+        a.set_xlabel("heure")
+        a.set_title("Valeur de BTC en € sur Kraken")
+        
+        canvas = FigureCanvasTkAgg(f, self)
+        canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.X, expand=True)  # recupere le widget tkinter de la figure canva.tk.get...
+        
+        button_updating_graph.bind("<ButtonRelease-1>", on_click_button_updating_graph)
+        button_updating_scrap.bind("<ButtonRelease-1>", on_click_button_updating_scrap)
         
         
         
